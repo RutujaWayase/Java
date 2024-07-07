b29539d3264426034ef8d44b9135397efabdac0c
RutujaWayase <rutujawayase1234@gmail.com>
2024-07-07 23:37:47 +0530

    adding operators

---
+++ JavaPract/src/test/java/OperatorsExpressions/Operators.java
@@ -0,0 +1,40 @@
+package OperatorsExpressions;
+
+public class Operators {
+	
+	public static void main(String[] args) {
+		
+		//1)Arithmetic operators + - * / %
+		int a=20, b=10;
+		
+		int result=a+b;
+		
+		System.out.println("Sum of a and b is"+ result);
+		System.out.println("Sum of a and b is:"+(a+b));
+		System.out.println("Diff of a and b is:"+(a-b));
+		System.out.println("Multiplication of a and b is:"+(a*b));
+		System.out.println("Division of a and b is: "+(a/b));
+		System.out.println("Modulus of a and b is:"+(a%b));
+		
+		//2)Relational/comparison operator > >= < <= != ==
+		//returns boolean value -true/false
+		
+		System.out.println(a>b); //true
+		System.out.println(a<b); //false
+		System.out.println(a>=b); //true
+		System.out.println(a<=b); //false
+		b=20;
+		System.out.println(a<=b); //true
+		System.out.println(a>=b); //true
+		
+		System.out.println(a!=b); //false
+		System.out.println(a==b); //true
+		
+		boolean res=a>b;
+		System.out.println(res);
+		
+		//3)Logical operators && || 
+		//
+	}
+
+}
--- JavaPract/src/test/java/VariablesDatatypes/DataTypes.java
+++ JavaPract/src/test/java/VariablesDatatypes/DataTypes.java
@@ -22,6 +22,46 @@ package VariablesDatatypes;
 
 public class DataTypes {
 	
+	//Numeric data types
+	
+	int a=100, b=200;
+	System.out.println("the value of a: "+a);
+	System.out.println(a+b);
+	System.out.println("the sum of a and b is: "+(a+b));
+	
+	byte by=125;
+	System.out.println(by);
+	
+	short sh=3535;
+	System.out.println(sh);
+	
+	long l=123456L; //literal 'l' or 'L'
+	System.out.println(l);
+	
+	float item_price=15.5F; //literal 'f' or 'F'
+	System.out.println(item_price);
+	
+	double dbl=12334.234567;
+	System.out.println(dbl);
+	
+	char grad='A';
+	System.out.println(grad);
+	
+	String name="John";
+	System.out.println(name);
+	
+	//char ch='ABC'; //invalid
+	//String ch='ABC'; //invalid
+	//String ch='A'; //invalid
+	String ch="A"; //valid
+	
+	boolean b1 = false; //allows value as only true/false
+	System.out.println(b1);
+	
+	//boolean b1 = "true"; //invalid
+	//String b1 =true; //invalid
+	
+	String b1="true"; //valid
 	
 
 }
