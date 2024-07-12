5338c3bb0b7550b1ec15db001edcb565651dc97b
RutujaWayase <rutujawayase1234@gmail.com>
2024-07-12 06:12:56 +0530

    adding oops

---
+++ JavaPract/src/test/java/oopsMethodsConstructors/Greetings.java
@@ -0,0 +1,34 @@
+package oopsMethodsConstructors;
+
+public class Greetings {
+	
+	
+	
+	
+	//1)No params  No return value
+	
+	void m1() {
+		System.out.println("Hello");
+	}
+	
+	//2)No params  Return value
+	
+		String m2() {
+			return("Hello how are ypu");
+		}
+
+		//3)Takes params  No return value
+		
+		void m3(String name) {
+			System.out.println("Hello"+name);
+		}
+
+		//4)Take params  Return value
+		
+		String m4(String name) {
+			return("Hello"+name);
+		}
+
+			
+
+}
---
+++ JavaPract/src/test/java/oopsMethodsConstructors/GreetingsMain.java
@@ -0,0 +1,20 @@
+package oopsMethodsConstructors;
+
+public class GreetingsMain {
+	
+	public static void main(String[] args) {
+		Greetings gr=new Greetings();
+		
+		gr.m1();
+		
+		String s = gr.m2();
+		System.out.println(s);
+		
+		gr.m3("John");
+		
+		String var=gr.m4("David");
+		System.out.println(var);
+		
+	}
+
+}
