18608caabcf9d25241b400c7da2545ba3fa2a1d5
RutujaWayase <rutujawayase1234@gmail.com>
2024-07-13 20:36:23 +0530

    adding swapping

---
+++ JavaPract/src/test/java/JavaPograms/SwapTwoNumbers.java
@@ -0,0 +1,17 @@
+ package JavaPograms;
+
+public class SwapTwoNumbers {
+	
+	public static void main(String[] args) {
+		int a=10, b=20;
+		System.out.println("Before swapping values are: "+a+ " "+b);
+		
+		//logic 1 - third value
+		int t=a;
+		a=b;
+		b=t;
+		
+		System.out.println("After swapping values are: "+a+ " "+b);
+	}
+
+}
