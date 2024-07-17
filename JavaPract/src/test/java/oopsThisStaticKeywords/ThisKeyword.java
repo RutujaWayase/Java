ad612b5158ffc386f6177c232a58bdda101ab430
RutujaWayase <rutujawayase1234@gmail.com>
2024-07-17 23:59:44 +0530

    adding this keyword oop

--- JavaPract/src/test/java/oopsThisStaticKeywords/ThisKeyword.java
+++ JavaPract/src/test/java/oopsThisStaticKeywords/ThisKeyword.java
@@ -1,5 +1,23 @@
 package oopsThisStaticKeywords;
 
 public class ThisKeyword {
+	int x,y; //class variables/instance variables
+	
+	ThisKeyword(int a, int b){
+		x=a;
+		y=b;
+	}
+	
+	void display() {
+		System.out.println(x);
+		System.out.println(y);
+	}
+	
+	public static void main(String[] args) {
+		
+		ThisKeyword th = new ThisKeyword(100,200);
+		th.display();
+		
+	}
 
 }
