67205fec608172adebf64ba43842e2e1575ba27f
RutujaWayase <rutujawayase1234@gmail.com>
2025-01-02 23:59:49 +0530

    adding numbers

---
+++ JavaPract/src/test/java/numbers/FibonacciSeries.java
@@ -0,0 +1,9 @@
+package numbers;
+
+public class FibonacciSeries {
+    public static void main(String[] args){
+        int num1 = 0, num2 = 1, num = 10;
+        
+    }
+    
+}
---
+++ JavaPract/src/test/java/patterns/NumberPattern.java
@@ -0,0 +1,38 @@
+package patterns;
+
+public class NumberPattern {
+    public static void main(String[] args) {
+    /* 
+        int n=5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    
+    */
+
+    //numbers
+   /*
+    int n=5;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            System.out.println("1");
+        }
+        System.out.println();
+    }
+    */
+    
+    //increasing numbers
+    int n=5;
+    for(int p=5; p<=n; p++){
+        for(int j=1; j<=p; j++){
+            System.out.println(p+""); 
+        }
+        System.out.println();
+    }
+
+    }
+    
+}
